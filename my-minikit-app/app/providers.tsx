7e5d7409f1d79cd665297bd0502cca1553bae464
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-08-29 23:31:21 +0900

    軽微な修正

--- my-minikit-app/app/providers.tsx
+++ my-minikit-app/app/providers.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { type ReactNode } from 'react';
-import { base } from 'wagmi/chains';
 import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
+import { type ReactNode } from 'react';
+import { baseSepolia } from 'wagmi/chains';
 
 export function Providers(props: { children: ReactNode }) {
   return (
     <MiniKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
+      chain={baseSepolia}
       config={{
         appearance: {
           mode: 'auto',
